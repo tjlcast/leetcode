518cd593b43a8e0b862f15e092eff60a7a24343e
tjlcast <phx108@sina.com>
2016-08-10 10:22:27 +0800

    Create Clone_Graph_133.java
    for leetcode 133

---
+++ Clone_Graph_133.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        
+    }
+    
+    private void work(UndirectedGraphNode node) {
+        
+    }
+    
+    private void bfs(UndirectedGraphNode root) {
+        list<UndirectedGraphNode> recoder = new list<UndirectedGraphNode>() ;
+        list.add(root) ;
+        
+        while(!root.empty()) {
+            cur_node = roo.
+        }
+    }
+}
