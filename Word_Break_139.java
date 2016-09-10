4dd761c4db72ce904c81bb5581dc037cc52134e3
tjlcast <phx108@sina.com>
2016-09-10 12:03:29 +0800

    success_V

---
+++ Word_Break_139.java
@@ -0,0 +1,35 @@
+public class Solution {
+
+    private String __author__ = "tangjialiang" ;
+    private String __V__ = "success_V" ;
+
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        return new Solution1().work(s, wordDict) ;
+    }
+}
+
+
+class Solution1 {
+    public boolean work(String s, Set<String> wordDict) {
+        if (wordDict.isEmpty()) return false ;
+        if (s.length()==0) return true ;
+        
+        int[] dp = new int[s.length() + 1] ;
+        dp[0] = 1 ;
+        
+        for (int i=1; i<=s.length(); i++) {
+            for (int j=0; j<i; j++) {
+                if (dp[j]==1 && (wordDict.contains(s.substring(j,i))==true)) {
+                    dp[i] = 1 ;
+                    break ;
+                }
+            }
+        }
+        
+        if(dp[s.length()]==1) {  
+            return true;  
+        } else {  
+            return false;  
+        }  
+    }
+}
