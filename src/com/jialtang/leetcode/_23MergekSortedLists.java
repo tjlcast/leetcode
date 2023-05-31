f63615e728dddff1c885421ce794baa20542a0ab
jialiang tang <jialiangtang@jialiangdeMacBook-Pro.local>
2023-05-31 17:25:49 +0800

    <ci>:Done 023.

---
+++ src/com/jialtang/leetcode/_23MergekSortedLists.java
@@ -0,0 +1,48 @@
+package com.jialtang.leetcode;
+
+import com.jialtang.leetcode.common.ListNode;
+
+public class _23MergekSortedLists {
+    public static void main(String[] args) {
+        ListNode node = ListNode.Create.createNode(3);
+        ListNode node2 = ListNode.Create.createNode(2);
+        ListNode node1 = ListNode.Create.createNode(4);
+        ListNode[] lists = new ListNode[]{node, node1, node2};
+
+        ListNode listNode = new Solution().mergeKLists(lists);
+        System.out.println(listNode);
+    }
+
+    static class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            ListNode root = new ListNode();
+            ListNode tail = root;
+
+            int pos = getMinPos(lists);
+            while (pos != -1) {
+                ListNode node = lists[pos];
+                lists[pos] = lists[pos].next;
+                tail.next = node;
+                tail = tail.next;
+                pos = getMinPos(lists);
+            }
+
+            return root.next;
+        }
+
+        private int getMinPos(ListNode[] lists) {
+            if (lists == null || lists.length == 0) {
+                return -1;
+            }
+            int pos = -1;
+            int val = Integer.MAX_VALUE;
+            for (int i = 0; i < lists.length; i++) {
+                if (lists[i] != null && lists[i].val <= val) {
+                    pos = i;
+                    val = lists[i].val;
+                }
+            }
+            return pos;
+        }
+    }
+}
