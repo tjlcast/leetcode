2c8d9ec37391caf42554cede1f336ed1f7b3163e
jialiang tang <jialiangtang@jialiangdeMacBook-Pro.local>
2023-05-31 12:09:29 +0800

    <ci>:Done 020.

--- src/com/jialtang/leetcode/_020ValidParentheses.java
+++ src/com/jialtang/leetcode/_020ValidParentheses.java
@@ -1,2 +1,61 @@
-package com.jialtang.leetcode;public class _020ValidParentheses {
+package com.jialtang.leetcode;
+
+import java.util.Stack;
+
+public class _020ValidParentheses {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        boolean valid = new Solution().isValid(s);
+        System.out.println(valid);
+
+        String s1 = "(]{}";
+        boolean valid1 = new Solution().isValid(s1);
+        System.out.println(valid1);
+
+        String s2 = "([)]";
+        boolean valid2 = new Solution().isValid(s2);
+        System.out.println(valid2);
+    }
+
+    static class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                Character peek = (stack.isEmpty()) ? ('0') : (stack.peek());
+                switch (c) {
+                    case ')':
+                        if (peek.equals('(')) {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case ']':
+                        if (peek.equals('[')) {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                        break;
+                    case '}':
+                        if (peek.equals('{')) {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                        break;
+                    default:
+                        if (!(c == '(' || c == '[' || c == '{')) {
+                            return false;
+                        }
+                        stack.add(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                return true;
+            }
+            return false;
+        }
+    }
 }
