957409d78fed22c9008415a4f6b5c724b6fc9815
jialiang tang <jialiangtang@jialiangdeMacBook-Pro.local>
2023-06-04 17:28:24 +0800

    <ci>:Done 037.

--- src/com/jialtang/leetcode/_037SudokuSolver.java
+++ src/com/jialtang/leetcode/_037SudokuSolver.java
@@ -1,2 +1,73 @@
-package com.jialtang.leetcode;public class _037SudokuSolver {
+package com.jialtang.leetcode;
+
+import java.util.Arrays;
+
+public class _037SudokuSolver {
+  public static void main(String[] args) {
+    char[][] board =
+        new char[][] {
+          {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+          {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+          {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+          {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+          {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+          {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+          {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+          {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+          {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+    new Solution().solveSudoku(board);
+    for (int i = 0; i < board.length; i++) {
+      System.out.println(Arrays.toString(board[i]));
+    }
+  }
+
+  static class Solution {
+    private final int n = 9;
+    private boolean[][] rows = new boolean[n][n];
+    private boolean[][] cols = new boolean[n][n];
+    private boolean[][] blks = new boolean[n][n];
+
+    public void solveSudoku(char[][] board) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (board[i][j] == '.') continue;
+          int num = board[i][j] - '1';
+          rows[i][num] = true;
+          cols[j][num] = true;
+          blks[i / 3 * 3 + j / 3][num] = true;
+        }
+      }
+      backtrace(board, 0, 0);
+    }
+
+    private boolean backtrace(char[][] board, int i, int j) {
+      if (i == board.length) return true;
+
+      // 本行结束，定位到下一行开始位置
+      if (j == board.length) return backtrace(board, i + 1, 0);
+      // skip.
+      if (board[i][j] != '.') return backtrace(board, i, j + 1);
+
+      int m = i / 3 * 3 + j / 3;
+      for (int k = 0; k < 9; k++) {
+        if (!rows[i][k] && !cols[j][k] && !blks[m][k]) {
+          rows[i][k] = true;
+          cols[j][k] = true;
+          blks[m][k] = true;
+          board[i][j] = (char) (k + '1');
+
+          if (backtrace(board, i, j + 1)) {
+            return true;
+          }
+
+          board[i][j] = '.';
+          rows[i][k] = false;
+          cols[j][k] = false;
+          blks[m][k] = false;
+        }
+      }
+      return false;
+    }
+  }
 }
--- src/com/jialtang/leetcode/common/practice/NSum.java
+++ src/com/jialtang/leetcode/common/practice/NSum.java
@@ -1,2 +1,39 @@
-package com.jialtang.leetcode.common.practice;public class NSum {
+package com.jialtang.leetcode.common.practice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NSum {
+  public static void main(String[] args) {
+    int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+    int target = 6;
+
+    List<List<Integer>> sum = new Solution().sum(nums, target);
+    System.out.println(sum);
+  }
+
+  static class Solution {
+    List<List<Integer>> ans;
+
+    List<List<Integer>> sum(int[] nums, int target) {
+      ans = new LinkedList<>();
+      dfs(nums, nums.length - 1, target, new ArrayList<>());
+      return ans;
+    }
+
+    void dfs(int[] nums, int pos, int target, List<Integer> list) {
+      if (pos < 0) return;
+      if (target < 0) return;
+
+      if (target == 0) {
+        ans.add(new ArrayList<>(list));
+      } else {
+        dfs(nums, pos - 1, target, new ArrayList<>(list));
+        ArrayList<Integer> integers = new ArrayList<>(list);
+        integers.add(nums[pos]);
+        dfs(nums, pos - 1, target - nums[pos], integers);
+      }
+    }
+  }
 }
