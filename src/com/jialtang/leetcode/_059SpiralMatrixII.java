3575468d8b80aef47f3f2aff7bcffe75fa4164f8
jialiang tang <jialiangtang@jialiangdeMacBook-Pro.local>
2023-06-07 21:50:57 +0800

    <ci>:Done 059.

--- src/com/jialtang/leetcode/_059SpiralMatrixII.java
+++ src/com/jialtang/leetcode/_059SpiralMatrixII.java
@@ -6,8 +6,12 @@ public class _059SpiralMatrixII {
   public static void main(String[] args) {
     int n;
     int[][] ints;
-    n = 3;
 
+    n = 3;
+    ints = new Solution().generateMatrix(n);
+    Arrays.stream(ints).forEach(e -> System.out.println(Arrays.toString(e)));
+
+    n = 1;
     ints = new Solution().generateMatrix(n);
     Arrays.stream(ints).forEach(e -> System.out.println(Arrays.toString(e)));
   }
@@ -19,15 +23,36 @@ public class _059SpiralMatrixII {
     public int[][] generateMatrix(int num) {
       int n = num * num;
       matrix = new int[num][num];
+      int d = 0;
 
       int x = 0;
       int y = 0;
       matrix[x][y] = 1;
-      for (int i = 2; i < n; i++) {
-
+      for (int i = 2; i <= n; i++) {
+        int k = 0;
+        for (; k < 4; k++) {
+          int xx = x + directions[d][0];
+          int yy = y + directions[d][1];
+          if (can(xx, yy)) {
+            matrix[xx][yy] = i;
+            x = xx;
+            y = yy;
+            break;
+          } else {
+            d = (d + 1) % 4;
+          }
+        }
+        if (k >= 4) break;
       }
 
       return matrix;
+    }
+
+    private boolean can(int xx, int yy) {
+      if (xx < 0 || xx >= matrix.length) return false;
+      if (yy < 0 || yy >= matrix[0].length) return false;
+      if (matrix[xx][yy] != 0) return false;
+      return true;
     }
   }
 }
