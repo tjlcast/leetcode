8dfe24bd74e5b61155f163e3458f69487d3cb9ab
tjlcast <phx108@sina.com>
2023-06-07 23:23:18 +0800

    <ci>:Done 061.

---
+++ src/com/jialtang/leetcode/_061RotateList.java
@@ -0,0 +1,51 @@
+package com.jialtang.leetcode;
+
+import com.jialtang.leetcode.common.ListNode;
+
+public class _061RotateList {
+  public static void main(String[] args) {
+    ListNode node, listNode;
+
+    node = ListNode.Create.createNode(3);
+    listNode = new Solution().rotateRight(node, 2000000000);
+    System.out.println(listNode);
+
+        node = ListNode.Create.createNode(5);
+        listNode = new Solution().rotateRight(node, 2);
+        System.out.println(listNode);
+  }
+
+  static class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+      if (head == null) return head;
+
+      ListNode root = new ListNode();
+      root.next = head;
+
+      int len = getLen(head);
+      k = k % len;
+
+      for (int i = 0; i < k; i++) {
+        ListNode node = root;
+        while (node.next.next != null) node = node.next;
+
+        ListNode lastNode = node.next;
+        node.next = null;
+
+        lastNode.next = root.next;
+        root.next = lastNode;
+      }
+
+      return root.next;
+    }
+
+    private int getLen(ListNode node) {
+      int num = 0;
+      while (node != null) {
+        node = node.next;
+        num += 1;
+      }
+      return num;
+    }
+  }
+}
