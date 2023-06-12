432e7e0323a3f6bbd6e008eaca9cc8ccbc6769c5
jialiang tang <jialiangtang@jialiangdeMacBook-Pro.local>
2023-06-12 16:49:45 +0800

    <ci>:Format 78.

--- src/com/jialtang/leetcode/_078Subsets.java
+++ src/com/jialtang/leetcode/_078Subsets.java
@@ -6,34 +6,34 @@ import java.util.List;
 
 public class _078Subsets {
 
-    public static void main(String[] args) {
-        int[] nums;
-        List<List<Integer>> ans;
+  public static void main(String[] args) {
+    int[] nums;
+    List<List<Integer>> ans;
 
-        nums = new int[] {1, 2, 3};
-        ans = new Solution().subsets(nums);
-        ans.stream().forEach(e -> System.out.println(Arrays.toString(e.toArray())));
-    }
+    nums = new int[] {1, 2, 3};
+    ans = new Solution().subsets(nums);
+    ans.stream().forEach(e -> System.out.println(Arrays.toString(e.toArray())));
+  }
 
-    static class Solution {
-        int[] nums;
-        List<List<Integer>> ans = new LinkedList<>();
+  static class Solution {
+    int[] nums;
+    List<List<Integer>> ans = new LinkedList<>();
 
-        public List<List<Integer>> subsets(int[] nums) {
-            this.nums = nums;
-            ans.add(new LinkedList());
+    public List<List<Integer>> subsets(int[] nums) {
+      this.nums = nums;
+      ans.add(new LinkedList());
 
-            for (int i=0; i<nums.length; i++) {
-                int n = nums[i];
-                int setSize = ans.size();
-                for (int j=0; j<setSize; j++) {
-                    LinkedList<Integer> item = new LinkedList(ans.get(j));
-                    ans.add(item);
-                    item.add(n);
-                }
-            }
-
-            return ans;
+      for (int i = 0; i < nums.length; i++) {
+        int n = nums[i];
+        int setSize = ans.size();
+        for (int j = 0; j < setSize; j++) {
+          LinkedList<Integer> item = new LinkedList(ans.get(j));
+          ans.add(item);
+          item.add(n);
         }
+      }
+
+      return ans;
     }
+  }
 }
