28b99e98c17c3516f688bba8a748d05c8cf5063f
jialiang tang <jialiangtang@jialiangdeMacBook-Pro.local>
2023-06-13 21:16:54 +0800

    <ci>:Done 100.

---
+++ src/com/jialtang/leetcode/_100SameTree.java
@@ -0,0 +1,18 @@
+package com.jialtang.leetcode;
+
+import com.jialtang.leetcode.common.TreeNode;
+
+public class _100SameTree {
+    public static void main(String[] args) {}
+
+    static class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p==null && q==null) return true;
+            if (p==null && q!=null) return false;
+            if (p!=null && q==null) return false;
+
+            if (p.val != q.val) return false;
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
